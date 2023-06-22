af4771515de99f0019b83ea952f71cd41e9a1d0c
YY Wang <yywangzc@gmail.com>
2023-06-22 17:14:55 +0800

    version_3

--- read.py
+++ read.py
@@ -21,6 +21,14 @@ for d in data:
 
 print('留言的平均長度為', sum_len/len(data))
 
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
+print(new[0])
+print(new[1])
+
 
 
 
